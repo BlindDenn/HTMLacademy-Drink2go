57533d4a6e00a6a43222cf800a7a359d13fdf51b
BlindDenn <104034214+BlindDenn@users.noreply.github.com>
2023-06-25 13:16:54 +0300

    add card-sorter

--- source/js/range-slider.js
+++ source/js/range-slider.js
@@ -1,18 +1,41 @@
 const rangeSlider = document.getElementById('slider');
+const input0 = document.getElementById('input-0');
+const input1 = document.getElementById('input-1');
+const inputs = [input0, input1];
 
 const initSlider = () => {
   if (rangeSlider) {
-    console.log(rangeSlider);
+
     noUiSlider.create(rangeSlider, {
-      start: [20, 80],
+      start: [0, 900],
       connect: true,
       step: 1,
       range: {
-        'min': 0,
-        'max': 100
+        'min': [0],
+        'max': [999]
       }
     });
   };
+
+  input0.value = 100;
+
+  rangeSlider.noUiSlider.on('update', (values, handle) => {
+    // input0.value = 100;
+    inputs[handle].value = Math.round(values[handle]);
+  });
+
+  const setRangeSlider = (i, value) => {
+    let arr = [null, null];
+    arr[i] = value;
+
+    rangeSlider.noUiSlider.set(arr)
+  };
+
+  inputs.forEach((el, index) => {
+    el.addEventListener('change', (evt) => {
+      setRangeSlider(index, evt.target.value);
+    });
+  });
 }
 
 export { initSlider };

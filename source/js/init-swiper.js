145130e6775f3d24694bf83e7fc826f18db74f49
BlindDenn <104034214+BlindDenn@users.noreply.github.com>
2023-06-25 15:59:00 +0300

    clean warnings

--- source/js/init-swiper.js
+++ source/js/init-swiper.js
@@ -1,7 +1,10 @@
 const initSwiper = () => {
   var swiper = new Swiper(".swiper", {
+    grabCursor: true,
+    autoHeight: false,
     pagination: {
       el: ".swiper-pagination",
+      clicable: !0,
     },
     navigation: {
       nextEl: ".swiper-button-next",
@@ -11,3 +14,33 @@ const initSwiper = () => {
 };
 
 export { initSwiper };
+
+// import Swiper from "./vendor/swiper-bundle.js";
+// const activateSwiper = ()=>{
+//     new Swiper(".swiper",{
+//         loop: !0,
+//         grabCursor: !0,
+//         pagination: {
+//             el: ".swiper__pagination",
+//             bulletElement: "button",
+//             bulletClass: "swiper__bullet button",
+//             bulletActiveClass: "swiper__bullet--active",
+//             clickable: !0
+//         },
+//         navigation: {
+//             nextEl: ".swiper__button--next",
+//             prevEl: ".swiper__button--prev"
+//         },
+//         wrapperClass: "swiper__wrapper",
+//         slideClass: "swiper__slide",
+//         slideActiveClass: "swiper__slide--active",
+//         slideNextClass: "swiper__slide--next",
+//         slidePrevClass: "swiper__slide--prev",
+//         autoplay: {
+//             delay: 5e3,
+//             disableOnInteraction: !0
+//         }
+//     })
+// }
+// ;
+// export {activateSwiper};
